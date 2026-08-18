40f015db96d02cc27466853e7077521dfd0b73a9
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-08-18 14:51:58 -0300

    Update send-temp.sh

--- scripts/send-temp.sh
+++ scripts/send-temp.sh
@@ -3,4 +3,4 @@
 TEMP=${1:-"22.5"}
 
 echo "Enviando temperatura: $TEMP °C al tópico ingSoft/informa..."
-docker compose -f docker/docker-compose.yml exec -T mosquitto mosquitto_pub -h localhost -p 1883 -t "ingSoft/informa" -m "Temperatura actual: $TEMP °C"
+curl -d "Temperatura actual: $TEMP °C" "mqtt://localhost:1883/ingSoft/informa"
