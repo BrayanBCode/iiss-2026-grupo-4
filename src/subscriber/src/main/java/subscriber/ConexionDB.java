862f5a5e46ef3eebfe31ab63cd559af82469754b
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-08-31 16:12:01 -0300

    feat: Mas comentarios

--- src/subscriber/src/main/java/subscriber/ConexionDB.java
+++ src/subscriber/src/main/java/subscriber/ConexionDB.java
@@ -4,12 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// Metodo singletone
 public class ConexionDB {
 
+    // Variable estatica solo accesible por la clase
     private static Connection connection;
 
+    // Función publica estática que interactúa con el resto del codigo brindado siempre la misma conexion
     public static Connection get() throws SQLException {
         if (connection == null || connection.isClosed()) {
+            // Variables de entorno definidas en docker-compose: subscriber/enviroment
             String url = System.getenv("DB_URL");
             String user = System.getenv("DB_USER");
             String pass = System.getenv("DB_PASSWORD");
