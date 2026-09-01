a7894ed5b89aa6e48f37900417ba2126e7deea21
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-09-01 18:21:59 -0300

    refactor: send-temp.sh

--- scripts/send-temp.sh
+++ scripts/send-temp.sh
@@ -2,5 +2,5 @@
 # Si se pasa un argumento ($1), usa ese valor; si no, usa "22.5"
 TEMP=${1:-"22.5"}
 
-echo "Enviando temperatura: $TEMP °C al tópico ingSoft/informa..."
-curl -d "Temperatura actual: $TEMP °C" "mqtt://localhost:1883/ingSoft/informa"
+echo "Enviando temperatura: $TEMP °C al tópico shellyhtg3-a1b2c3d4e5f6/status/temperature:0..."
+curl -d "{"id":0,"tC":$TEMP,"tF":71.3,"ts":1788297466.028}" "mqtt://localhost:1883/shellyhtg3-a1b2c3d4e5f6/status/temperature:0"
